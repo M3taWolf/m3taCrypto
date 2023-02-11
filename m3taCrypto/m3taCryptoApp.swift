51f162401b3161002d5bee926c139104ec14f2f6
bekirkus <bekir.kus@hotmail.de>
2023-02-12 00:14:41 +0100

    Initial Commit

---
+++ m3taCrypto/m3taCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  m3taCryptoApp.swift
+//  m3taCrypto
+//
+//  Created by Bekir Kus on 12.02.23.
+//
+
+import SwiftUI
+
+@main
+struct m3taCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
